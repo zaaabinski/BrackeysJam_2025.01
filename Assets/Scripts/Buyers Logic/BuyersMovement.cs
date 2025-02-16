8704aef3c1537f888ff437615874d49f0e3263f1
Zaaabinski <jakub.zlobinski03@gmail.com>
2025-02-16 16:17:23 +0100

    Small fixes
    Buyers and ghost now stop in radious instead of exact point, collisions are working now, you can see when buyer is scared

--- Assets/Scripts/Buyers Logic/BuyersMovement.cs
+++ Assets/Scripts/Buyers Logic/BuyersMovement.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.AI;
 using Unity.AI.Navigation;
 using System.Linq;
@@ -22,13 +21,14 @@ public class BuyersMovement : MonoBehaviour
     [SerializeField] private LayerMask _anomalyDetectionMask;
 
     [SerializeField] private float _resumeMovingDelay;
-
+    [SerializeField] private float stoppingRadius = 2f; // Buyer stops within this radius
+    
     #endregion
 
-    #region 
+    #region
 
-    public bool IsScared { get; private set; }
-
+    [SerializeField] private bool IsScared = false;
+    [SerializeField] private GameObject scaredMark;
     #endregion
 
     private void Awake()
@@ -38,7 +38,7 @@ public class BuyersMovement : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(RandomAnomolyDestination());
+        StartCoroutine(RandomAnomalyDestination());
     }
 
     private void Update()
@@ -47,30 +47,48 @@ public class BuyersMovement : MonoBehaviour
         Collider[] surroundingColliders = Physics.OverlapSphere(transform.position, _anomalyDetectionRadius, _anomalyDetectionMask, QueryTriggerInteraction.Collide);
         Collider[] detectedAnomalies = surroundingColliders.Where(x => x.CompareTag("Anomaly")).ToArray();
 
-        if (detectedAnomalies.Length > 0){
+        if (detectedAnomalies.Length > 0)
+        {
+            Vector3 anomalyPosition = detectedAnomalies[0].transform.position;
+            
+            // Offset the destination by a random point within the stopping radius
+            Vector3 offset = Random.insideUnitSphere * stoppingRadius;
+            offset.y = 0; // Keep buyer on the ground
+            Vector3 targetPosition = anomalyPosition + offset;
+
             // Make sure we aren't setting the destination to the same thing
-            if (_agent.destination != detectedAnomalies[0].transform.position){
-                _agent.SetDestination(detectedAnomalies[0].transform.position);
+            if (_agent.destination != targetPosition)
+            {
+                _agent.SetDestination(targetPosition);
             }
 
             IsScared = true;
+            scaredMark.SetActive(true);
 
-            return; // prevent it from going away from the anomaly before the player removes it
+            return; // Prevent moving away before the anomaly is removed
         }
 
         IsScared = false;
+        scaredMark.SetActive(false);
 
-        // If it reached its destination we reset it to another random point
-        if (_agent.remainingDistance <= _agent.stoppingDistance){
-            StartCoroutine(RandomAnomolyDestination());
+        // If it reached its destination within stopping radius, pick another destination
+        if (!_agent.pathPending && _agent.remainingDistance <= stoppingRadius)
+        {
+            StartCoroutine(RandomAnomalyDestination());
         }
     }
 
-    private IEnumerator RandomAnomolyDestination(){
+    private IEnumerator RandomAnomalyDestination()
+    {
         yield return new WaitForSeconds(_resumeMovingDelay);
 
         Vector3 point = _navmeshUtilities.GetRandomPointOnNavmesh(_navmeshSurface);
-
+        
+        // Offset the destination by a random point within the stopping radius
+        Vector3 offset = Random.insideUnitSphere * stoppingRadius;
+        offset.y = 0; // Keep buyer on the ground
+        point += offset;
+        
         _agent.SetDestination(point);
     }
 }
