53710b2ee2cc0e6152e86f21ea4adea13433aa2c
Zaaabinski <jakub.zlobinski03@gmail.com>
2025-02-16 16:52:19 +0100

    Fixed code conflict

--- Assets/Scripts/Buyers Logic/BuyersMovement.cs
+++ Assets/Scripts/Buyers Logic/BuyersMovement.cs
@@ -9,30 +9,47 @@ public class BuyersMovement : MonoBehaviour
     #region components
 
     private NavMeshAgent _agent;
+    private MeshRenderer _renderer;
     [SerializeField] private NavmeshUtilities _navmeshUtilities;
     [SerializeField] private NavMeshSurface _navmeshSurface;
+
+    [Header("Anomaly settings")]
+    [Tooltip(
+        "After investigating the anomaly it gets scared. After this many seconds it runs away if its still scared")]
+    [SerializeField]
+    private float _runAwayCooldown;
+
+    [SerializeField] Transform _getAwayPoint;
+
+    #endregion
+
+
+    #region private variables
+
+    private float _scaredTimer;
 
     #endregion
 
     #region settings
-    [Header("Settings")]
-    
-    [SerializeField] private float _anomalyDetectionRadius;
+
+    [Header("Settings")] [SerializeField] private float _anomalyDetectionRadius;
     [SerializeField] private LayerMask _anomalyDetectionMask;
 
     [SerializeField] private float _resumeMovingDelay;
     [SerializeField] private float stoppingRadius = 2f; // Buyer stops within this radius
-    
+
     #endregion
 
     #region
 
     [SerializeField] private bool IsScared = false;
     [SerializeField] private GameObject scaredMark;
+
     #endregion
 
     private void Awake()
     {
+        _renderer = GetComponent<MeshRenderer>();
         _agent = GetComponent<NavMeshAgent>();
     }
 
@@ -43,14 +60,35 @@ public class BuyersMovement : MonoBehaviour
 
     private void Update()
     {
+        if (IsScared)
+        {
+            _scaredTimer += Time.deltaTime;
+        }
+        else
+        {
+            _scaredTimer = 0;
+        }
+
+        if (_scaredTimer > _runAwayCooldown)
+        {
+            _renderer.material.color = Color.red;
+            if (_agent.destination != _getAwayPoint.position)
+            {
+                _agent.destination = _getAwayPoint.position;
+            }
+
+            return;
+        }
+
         // Check for any nearby anomalies
-        Collider[] surroundingColliders = Physics.OverlapSphere(transform.position, _anomalyDetectionRadius, _anomalyDetectionMask, QueryTriggerInteraction.Collide);
+        Collider[] surroundingColliders = Physics.OverlapSphere(transform.position, _anomalyDetectionRadius,
+            _anomalyDetectionMask, QueryTriggerInteraction.Collide);
         Collider[] detectedAnomalies = surroundingColliders.Where(x => x.CompareTag("Anomaly")).ToArray();
 
         if (detectedAnomalies.Length > 0)
         {
             Vector3 anomalyPosition = detectedAnomalies[0].transform.position;
-            
+
             // Offset the destination by a random point within the stopping radius
             Vector3 offset = Random.insideUnitSphere * stoppingRadius;
             offset.y = 0; // Keep buyer on the ground
@@ -61,6 +99,7 @@ public class BuyersMovement : MonoBehaviour
             {
                 _agent.SetDestination(targetPosition);
             }
+
 
             IsScared = true;
             scaredMark.SetActive(true);
@@ -83,12 +122,12 @@ public class BuyersMovement : MonoBehaviour
         yield return new WaitForSeconds(_resumeMovingDelay);
 
         Vector3 point = _navmeshUtilities.GetRandomPointOnNavmesh(_navmeshSurface);
-        
+
         // Offset the destination by a random point within the stopping radius
         Vector3 offset = Random.insideUnitSphere * stoppingRadius;
         offset.y = 0; // Keep buyer on the ground
         point += offset;
-        
+
         _agent.SetDestination(point);
     }
 }
