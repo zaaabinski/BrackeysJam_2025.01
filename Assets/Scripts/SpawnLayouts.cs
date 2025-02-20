c80a735375a51326e662588311cd23ecb5a5e9e5
Zaaabinski <jakub.zlobinski03@gmail.com>
2025-02-20 16:19:12 +0100

    Animations/Room layouts
    Added animations for player and buyer.
    Added the layouts of the rooms so that they spawn randomly

--- Assets/Scripts/SpawnLayouts.cs
+++ Assets/Scripts/SpawnLayouts.cs
@@ -1,29 +1,43 @@
 using System.Collections.Generic;
 using Unity.AI.Navigation;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class SpawnLayouts : MonoBehaviour
-{ 
+{
     [SerializeField] private NavMeshSurface navSurfaceBuyer;
     [SerializeField] private NavMeshSurface navSurfaceGhost;
-    
-   [Header("LayoutPlaces")]
-   [SerializeField] List<GameObject> layouts = new List<GameObject>();
 
-   [Header("Spawn Layouts")] 
-   [SerializeField] private GameObject[] roomTypeOne;
-   [SerializeField] private GameObject[] roomTypeTwo;
-   [SerializeField] private GameObject[] roomTypeThree;
-   [SerializeField] private GameObject[] roomTypeFour;
-   
-   private void Start()
-   {
-      foreach (GameObject roomType in layouts)
-      {
-          Instantiate(roomTypeOne[Random.Range(0,roomTypeOne.Length)], roomType.transform.position, Quaternion.identity);
-      }
-      navSurfaceBuyer.BuildNavMesh();
-      navSurfaceGhost.BuildNavMesh();
-   }
+    [Header("LayoutPlaces")] [SerializeField]
+    List<GameObject> layouts = new List<GameObject>();
+
+    [Header("Spawn Layouts")] [SerializeField]
+    private GameObject[] roomTypeOne;
+
+    [SerializeField] private GameObject[] roomTypeTwo;
+    [SerializeField] private GameObject[] roomTypeThree;
+    [SerializeField] private GameObject[] roomTypeFour;
+    [SerializeField] private GameObject[] roomTypeFive;
+
+    private void Start()
+    {
+        for (int i = 0; i < layouts.Count; i++)
+        {
+            GameObject[] selectedRoomType;
+
+            if (i == 0) selectedRoomType = roomTypeOne;
+            else if (i == 1) selectedRoomType = roomTypeTwo;
+            else if (i == 2) selectedRoomType = roomTypeThree;
+            else if (i == 3) selectedRoomType = roomTypeFour;
+            else if (i == 4) selectedRoomType = roomTypeFive;
+            else selectedRoomType = roomTypeFive;
+
+            if (selectedRoomType.Length > 0)
+            {
+                Instantiate(selectedRoomType[Random.Range(0, selectedRoomType.Length)]);
+            }
+
+            navSurfaceBuyer.BuildNavMesh();
+            navSurfaceGhost.BuildNavMesh();
+        }
+    }
 }
