4cdc5710912e401e4ff06d0f8999fb1bf7b51a66
AxelBorg1 <axelborg2009@gmail.com>
2025-02-21 19:59:13 +0100

    ghost delay before anomaly activation

--- Assets/Scripts/AnomalyActivation.cs
+++ Assets/Scripts/AnomalyActivation.cs
@@ -1,24 +1,43 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using Random = System.Random;
 
 public class AnomalyActivation : MonoBehaviour
 {
     [SerializeField] private int chanceForSpawn=25;
-    
+    [SerializeField] private float _delayBeforeApplyingAnomaly;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ghost"))
         {   
-            Random random = new Random();
+            StartCoroutine(ApplyAnomaly(other.gameObject));
+        }
+    }
 
-            if (random.Next(0, 100) < chanceForSpawn)
+    private IEnumerator ApplyAnomaly(GameObject ghost)
+    {
+        // Freeze ghost
+        NavMeshAgent agent = ghost.GetComponent<NavMeshAgent>();
+
+        float ghostSpeed = agent.speed;
+        agent.speed = 0;
+
+        yield return new WaitForSeconds(_delayBeforeApplyingAnomaly);
+
+        agent.speed = ghostSpeed;
+
+        // Apply anomaly
+        Random random = new Random();
+
+        if (random.Next(0, 100) < chanceForSpawn)
+        {
+            foreach (Transform child in transform)
             {
-                foreach (Transform child in transform)
-                {
-                    AnomalyScript anomalyScript = child.GetComponent<AnomalyScript>();
-                    anomalyScript.StartAnomaly();
-                }
+                AnomalyScript anomalyScript = child.GetComponent<AnomalyScript>();
+                anomalyScript.StartAnomaly();
             }
         }
     }
